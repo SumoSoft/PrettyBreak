ebb56cb9bf589591bf11b135444b00229c015132
Francesco Lorenzetti <18499692+FrancescoLorenzetti84@users.noreply.github.com>
2019-08-23 12:11:12 +0100

    Update jquery.pretty-break.js

--- jquery.pretty-break.js
+++ jquery.pretty-break.js
@@ -1,36 +1,26 @@
 $.fn.prettyBreak = function() {
-    
-		return this.each(function() {
-            
-			const element = $(this);
-            const elementContent = element.text();
-
-            element.find("br").remove();
-			element.wrapInner("<span style='white-space: nowrap'>");
-
-			const textWidth = element.find("span").width();
-
-			element.html(elementContent);
-
-			const elementText = $.trim(element.text());
-
-			if (textWidth > element.width()) {
-				let middle = Math.floor(elementText.length / 2);
-				const before = elementText.lastIndexOf(" ", middle);
-				const after = elementText.indexOf(" ", middle + 1);
-
-				if (middle - before < after - middle) {
-					middle = before;
-				} else {
-					middle = after;
-				}
-
-				const s1 = elementText.substr(0, middle);
-				const s2 = elementText.substr(middle + 1);
-
-				element.html(s1 + "<br>" + " " + s2); // note the space after the tag
+	return this.each(function() {
+		const element = $(this);
+		const elementContent = element.text();
+		element.find("br").remove();
+		element.wrapInner("<span style='white-space: nowrap'>");
+		const textWidth = element.find("span").width();
+		element.html(elementContent);
+		const elementText = $.trim(element.text());
+		if (textWidth > element.width()) {
+			let middle = Math.floor(elementText.length / 2);
+			const before = elementText.lastIndexOf(" ", middle);
+			const after = elementText.indexOf(" ", middle + 1);
+			if (middle - before < after - middle) {
+				middle = before;
 			} else {
-				element.html(elementText);
+				middle = after;
 			}
-		});
-	};
+			const s1 = elementText.substr(0, middle);
+			const s2 = elementText.substr(middle + 1);
+			element.html(s1 + "<br>" + " " + s2); // note the space after the tag
+		} else {
+			element.html(elementText);
+		}
+	});
+};
